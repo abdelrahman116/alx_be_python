08f290347146de249db16c8bd3df0b34c3a40bdf
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-12-24 18:18:18 +0200

    F to C and the opposite

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,18 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+degree = input("Enter the temperature to convert:")
+ForC = input("Is this temperature in Celsius or Fahrenheit? (C/F):")
+
+def convert_to_celsius(fahrenheit):
+    return ((fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR)
+
+def convert_to_fahrenheit(celsius):
+    return ((celsius*CELSIUS_TO_FAHRENHEIT_FACTOR)+32)
+
+
+if ForC == 'C':
+   print( degree,"°C","is",convert_to_fahrenheit(int(degree)), "°F")
+
+if ForC == 'F':
+   print( degree,"°F","is",convert_to_celsius(int(degree)), "°C")
