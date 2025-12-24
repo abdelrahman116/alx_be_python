01dc5b529d5233c47a196648ae3ef37de01a365a
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-12-24 18:03:56 +0200

    Date-Time

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ import datetime
 from datetime import timedelta 
 def display_current_datetime():
     current_date = datetime.datetime.now()
-    formatted_daytime = current_date.strftime("%Y-%m-%d %H: %M: %S")
+    formatted_daytime = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(formatted_daytime)
 
 
