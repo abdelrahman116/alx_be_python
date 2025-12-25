9eb150e3222c382f2d4512e38972f448099c5b30
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-12-25 13:04:10 +0200

    Calculator Test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,7 +9,7 @@ class Claculator_Test(unittest.TestCase):
         self.assertEqual(self.calc.subtract(self,5,5),0)
     def test_multiplication(self):
         self.assertEqual(self.calc.multiply(self,5,5),25)
-    def test_divide_normal(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(self,5,5),1)
     def test_divide_zero(self):
         self.assertEqual(self.calc.divide(self,5,0),None)
