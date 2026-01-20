44d19ac45efc5651c0e0df3ab31b1d23129b1a18
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2026-01-20 17:27:42 +0200

    Python

---
+++ oop/library_system.py
@@ -0,0 +1,19 @@
+import json
+
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+          return  f"{self.title} by {self.author}, published in {self.year}."
+    
+    def __repr__(self):
+            print(f"Book('{self.title}', '{self.author}', {self.year})")
+
+    def __del__ (self):
+            print("Deleting (title of the book)")
+        
+
+    
